ebc65e01c6e666ea1774f2310d0b6339e38460ce
Mark Cyster <mcyster@extole.com>
2026-06-28 10:58:18 -0700

    Update docker and nixpkgs

--- my.nix
+++ my.nix
@@ -1,5 +1,14 @@
 { config, pkgs, lib, ... }:
 
+let
+  dockerPackage = pkgs.docker_29.override {
+    version = "29.5.2";
+    cliRev = "v29.5.2";
+    cliHash = "sha256-kHgDZVr6mAyCtZ6bSG9FWV0GhWDfXLXzHYFrmjFzO9w=";
+    mobyRev = "docker-v29.5.2";
+    mobyHash = "sha256-lux7tTyF6vm5wuIXs+z3Ygd2v4JjgHbRvOXNA4kjNtg=";
+  };
+in
 {
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -36,6 +45,8 @@
     miller
     ngrok
 
+    wl-clipboard
+
     python3
     poetry
 
@@ -59,7 +70,10 @@
     nix-direnv.enable = true;
   };
 
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    package = dockerPackage;
+  };
 
   users.users.mcyster.extraGroups = [ "docker" ];
 
